f4c34e8392f5b31badea8fa7b4adfd4134a8b5ef
Leonardolimacavalcante890 <leonardolimacavalcante8@gmail.com>
2024-05-08 21:45:00 -0300

    primeira de aula de java

---
+++ Leonardo lima Cavalcante/JS/JS02-VARIAVEIS/main.js
@@ -0,0 +1,52 @@
+console.log("Hello World!")
+//quanto trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+
+//Variaveis
+//quero criar um espaço na meomoria do computador -felippe e dizer que esse espaço na memoria irá receber o valor 8
+var _felippe = 8; //variavel numerica
+var _felippe = 'Cristiano Ronaldo'; //variavel string //redeclarar uma variavel
+
+
+console.log(_felippe);
+
+let letsGo = 4
+letsGo = 6
+console.log(letsGo)
+console.clear()
+
+//const
+
+const PI = 3.1415;
+
+console.log(PI)
+
+console.clear()
+
+let NAN = 0 / 0;
+
+console.log(NAN)
+
+console.clear()
+
+//let IOS = "instituto da Oportunidade Social"; //variavel IOS recebeu uma string
+
+console.log("Instituto" + "da" + "Oportunidade" + "Social"); //concatenar: é juntar tudo em uma frase só, é possivel strings ou concatenar variaveis
+
+console.clear()
+var numero1 = 2
+var texto = "te amo"
+var texto2 = "sonho"
+
+console.log(texto + texto + numero1)
+console.clear()
+
+//valores booleanos
+
+var num1 = 1
+var num2 = 1
+//num1 é igual a num2
+//1 é igual a 1 = true
+//as duas variaveis recebe o mesmo valor
+
+console.log(num1 == num2)
+
