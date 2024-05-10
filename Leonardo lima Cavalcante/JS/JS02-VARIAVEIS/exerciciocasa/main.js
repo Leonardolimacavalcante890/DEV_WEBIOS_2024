31b490e0c6ec14de8cd6afbb56ccb18736b9d2ac
Leonardolimacavalcante890 <leonardolimacavalcante8@gmail.com>
2024-05-09 21:43:05 -0300

    atividade e liçao de java

---
+++ Leonardo lima Cavalcante/JS/JS02-VARIAVEIS/exerciciocasa/main.js
@@ -0,0 +1,8 @@
+var nota1 = 6;
+var nota2 = 5;
+var nota3 = 9;
+var nota4 = 10;
+var media = nota1 + nota2 + nota3 + nota4; 
+var media2 = media / 4;
+
+console.log("A media do aluno com as 4 notas é: " + media2)
--- Leonardo lima Cavalcante/JS/JS02-VARIAVEIS/main.js
+++ Leonardo lima Cavalcante/JS/JS02-VARIAVEIS/main.js
@@ -50,3 +50,69 @@ var num2 = 1
 
 console.log(num1 == num2)
 
+
+//operadores
+
+var operador1 = 8;
+var operador2= null;
+
+console.log(operador1 * operador2); //8 vezes 0
+
+var operador3 = '5';
+var operador4 = 1;
+
+console.log(operador3 - operador4); // "5" - 1 = 4
+
+var operador5 = "5";
+var operador6 = 1;
+
+console.log(operador5 + operador6)// '5' + 1 = 51
+
+var operador7 = 5;
+var operador8 = 1;
+
+console.log(operador7 + operador8)
+
+var operador9 = 'five';
+var operador10 = 2;
+
+console.log(operador9 * operador10) // 'five' vezes 2 = NaN porque sao letras e muitos caracteres
+console.clear()
+
+var valor = 2
+
+var valor1 = 2
+var resultado = valor % valor1;
+
+console.log(resultado)
+// var compare = 2 > 3 != 2 //false ou true
+var compara = 2 > 3 == 2 //false pq 2 nao é maior que 3 e dps ele verifica se 3 é igual a 2
+var teste = !compara //true pq 2 é diferente de 3 e 3 é diferente de 2
+
+
+console.log("variavel teste" + compara)
+console.log("variavel teste" + teste);
+//so esta comparando o valor
+//! é diferente
+//se o valor é menor ou igual
+//compara o valor e o tipo
+//só estA comparando o valor valor
+
+console.log(compara)
+
+console.clear()
+
+let valor3 = 4
+let valor4 = 6
+let resultado1 = valor3 < valor4 || valor3 == valor4
+//true e false == false
+//true e true == true
+// true e true == true
+//true OU flase == true
+
+console.log(resultado1)
+
+let valor5 = 4
+let valor6 = "6"
+
+console.log(typeof valor6) // tipo de valor
