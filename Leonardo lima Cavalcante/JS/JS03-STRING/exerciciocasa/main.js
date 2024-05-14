f260fb06c37a9d95dd3dcd8dd9cc4dc2606f4a84
Leonardolimacavalcante890 <leonardolimacavalcante8@gmail.com>
2024-05-13 21:48:37 -0300

    liçao de casa js3

---
+++ Leonardo lima Cavalcante/JS/JS03-STRING/exerciciocasa/main.js
@@ -0,0 +1,6 @@
+var texto =  "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log(texto[19]);
+console.log(`O texto tem ${texto.length} de caracteres`);
+console.log(`${texto.toLocaleUpperCase()}`);
+console.log(texto.substring(48));
