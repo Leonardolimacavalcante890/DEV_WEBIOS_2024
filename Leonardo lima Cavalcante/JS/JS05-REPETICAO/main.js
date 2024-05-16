874fe97958cffdd767091708fa07fd375c8950f5
Leonardolimacavalcante890 <leonardolimacavalcante8@gmail.com>
2024-05-15 21:41:40 -0300

    java05

---
+++ Leonardo lima Cavalcante/JS/JS05-REPETICAO/main.js
@@ -0,0 +1,25 @@
+//while >>  enquanto
+// Enquanto 0 < 10 vou apresentar a mensagem "0 ainda é menor que 10"
+
+var contador = 0
+
+//while(contador < 10) // o 10 é o limite do looping 
+//{
+ //   console.log(contador + " ainda é menor que 10")
+ //   debugger //é um pare do looping
+  //  contador++;
+    // contador = contador + 1
+    // contador = 0 + 1
+    //contador = 1
+//}
+
+do{
+    debugger
+    console.log(contador + " ainda é menor que 10")
+    contador++;
+}
+while(contador < 10)
+
+for(var contador = 10 ; contador < 10 ; contador++){
+    console.log(contador + "ainda é menor que 10")
+}
