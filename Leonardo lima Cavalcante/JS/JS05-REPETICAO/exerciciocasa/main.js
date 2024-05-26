b08e2fc7e5a6194c4e91960f9a26fa94219a1699
Leonardolimacavalcante890 <leonardolimacavalcante8@gmail.com>
2024-05-26 17:10:36 -0300

    lição java07 pronta

--- Leonardo lima Cavalcante/JS/JS05-REPETICAO/exerciciocasa/main.js
+++ Leonardo lima Cavalcante/JS/JS05-REPETICAO/exerciciocasa/main.js
@@ -1,7 +1,7 @@
 
 var contador = 1
 
- for(var contador = 1; contador <= 10 ; contador++){
+ for(var contador = 0; contador <= 10 ; contador++){
     console.log(`2 x ${contador} = ${2 * contador}`)
     debugger
     }
